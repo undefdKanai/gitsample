62ea44359ad591a9a87103b396245fdc4d4a2a87
yukiwakatsui <wakatsuki8000@gmail.com>
2017-12-02 16:26:44 +0900

    UIColor Change

--- gitsample/ViewController.swift
+++ gitsample/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        view.backgroundColor = UIColor.red
+        view.backgroundColor = UIColor.blue
     }
 
     override func didReceiveMemoryWarning() {
