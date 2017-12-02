21c86e2fbd926c70a2a3ed3c98f5ca0c21d8f69e
渡邉健嗣 <kenji@watanabekenjinomacbook-pro.local>
2017-12-02 16:33:35 +0900

    コメント

--- gitsample/ViewController.swift
+++ gitsample/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         view.backgroundColor = UIColor.black
+        //こんにちは
     }
 
     override func didReceiveMemoryWarning() {
